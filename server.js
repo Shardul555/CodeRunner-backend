25ae606830904df0b933ae09ac55ffdfbe092a43
Shardul555 <semwalshardul@gmail.com>
2022-10-17 21:45:12 +0530

    allow cors from frontent application

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 8080
 
+// cors configuration
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+
+// APIs
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.post('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// run server
 app.listen(port, () => {
     console.log(`Coderunner backend listening on port ${port}`)
 })
